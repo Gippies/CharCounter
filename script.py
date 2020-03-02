a7d811dca2dc05a8d176eaa57022c09028fa0ced
Joshua Patterson <jleegippies@gmail.com>
2020-03-01 19:13:34 -0600

    Added single process counter

--- script.py
+++ script.py
@@ -1,3 +1,20 @@
 
+
+def single_process_counter():
+    lines = []
+    with open('input.txt') as f:
+        for line in f:
+            lines.append(line)
+    count_dict = {}
+    for line in lines:
+        for char in line:
+            if char in count_dict:
+                count_dict[char] += 1
+            else:
+                count_dict[char] = 1
+
+    print(count_dict)
+
+
 if __name__ == '__main__':
-    pass
+    single_process_counter()
